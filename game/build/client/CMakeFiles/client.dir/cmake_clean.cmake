18186ee108c24c1ed11a2ed2f0753d210dea948f
Yaroslav <AzisHop777@mail.ru>
2020-05-12 14:53:41 +0300

    Animation of pig-police

---
+++ game/build/client/CMakeFiles/client.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/client.dir/Client.cpp.o"
+  "CMakeFiles/client.dir/Object.cpp.o"
+  "CMakeFiles/client.dir/Player.cpp.o"
+  "CMakeFiles/client.dir/Skin.cpp.o"
+  "CMakeFiles/client.dir/TmxLevel.cpp.o"
+  "CMakeFiles/client.dir/__/common/User.cpp.o"
+  "CMakeFiles/client.dir/__/messages/ServerToUserMessage.cpp.o"
+  "CMakeFiles/client.dir/__/messages/SessionCreatedMessage.cpp.o"
+  "CMakeFiles/client.dir/__/messages/UserInitMessage.cpp.o"
+  "CMakeFiles/client.dir/__/messages/UserToServerMessage.cpp.o"
+  "CMakeFiles/client.dir/main.cpp.o"
+  "CMakeFiles/client.dir/tinyxml2.cpp.o"
+  "client"
+  "client.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- game/client/Skin.cpp
+++ game/client/Skin.cpp
@@ -7,7 +7,7 @@
 
 
     Skin::Skin() {
-    heroimage.loadFromFile("/home/yaroslav/X/Project_X/game/images/hero.png");
+    heroimage.loadFromFile("../../images/pigcop_vest.png");
     herotexture.loadFromImage((heroimage));
     herosprite.setTexture(herotexture);
     herosprite.setTextureRect(sf::IntRect(0,192,96,96));
@@ -21,30 +21,30 @@
 void Skin::go_texture(Direction dir, float time, float& current_frame, Skin& m_actor) {
     if (dir.left) { //если нажата клавиша стрелка влево или англ буква А
         current_frame += 12*time; //служит для прохождения по "кадрам". переменная доходит до трех суммируя произведение времени и скорости. изменив 0.005 можно изменить скорость анимации
-        if (current_frame > 3) current_frame -= 3; //проходимся по кадрам с первого по третий включительно. если пришли к третьему кадру - откидываемся назад.
-        m_actor.herosprite.setTextureRect(IntRect(96 * int(current_frame), 96, 96, 96)); //проходимся по координатам Х. получается 96,96*2,96*3 и опять 96
+        if (current_frame > 2) current_frame -= 2; //проходимся по кадрам с первого по третий включительно. если пришли к третьему кадру - откидываемся назад.
+        m_actor.herosprite.setTextureRect(IntRect(16+82 , 105 * int(current_frame), 85 , 95)); //проходимся по координатам Х. получается 96,96*2,96*3 и опять 96
         //std::cout << "llllllllllllllllllllll" << "\n";
     }
 
     if (dir.right) {
         current_frame += 12*time; //служит для прохождения по "кадрам". переменная доходит до трех суммируя произведение времени и скорости. изменив 0.005 можно изменить скорость анимации
-        if (current_frame > 3) current_frame -= 3; //проходимся по кадрам с первого по третий включительно. если пришли к третьему кадру - откидываемся назад.
-        m_actor.herosprite.setTextureRect(IntRect(96 * int(current_frame), 192, 96, 96)); //проходимся по координатам Х. получается 96,96*2,96*3 и опять 96
+        if (current_frame > 2) current_frame -= 2; //проходимся по кадрам с первого по третий включительно. если пришли к третьему кадру - откидываемся назад.
+        m_actor.herosprite.setTextureRect(IntRect((16+82*2) , 105 * int(current_frame), -85 , 95)); //проходимся по координатам Х. получается 96,96*2,96*3 и опять 96
         // std::cout << "rrrrrrrrrrrrrr" << "\n";
     }
 
     if (dir.up) {
         current_frame += 11*time; //служит для прохождения по "кадрам". переменная доходит до трех суммируя произведение времени и скорости. изменив 0.005 можно изменить скорость анимации
-        if (current_frame > 3) current_frame -= 3; //проходимся по кадрам с первого по третий включительно. если пришли к третьему кадру - откидываемся назад.
-        m_actor.herosprite.setTextureRect(IntRect(96 * int(current_frame), 288, 96, 96)); //проходимся по координатам Х. получается 96,96*2,96*3 и опять 96
+        if (current_frame > 2) current_frame -= 2; //проходимся по кадрам с первого по третий включительно. если пришли к третьему кадру - откидываемся назад.
+        m_actor.herosprite.setTextureRect(IntRect((24+82*9) , 105 * int(current_frame), 90 , 95)); //проходимся по координатам Х. получается 96,96*2,96*3 и опять 96
         //std::cout << "uuuuuuuuuuuuu" << "\n";
         //std::cout << current_frame << "  BLABLA   \n";
     }
 
     if (dir.down) {
         current_frame += 11*time; //служит для прохождения по "кадрам". переменная доходит до трех суммируя произведение времени и скорости. изменив 0.005 можно изменить скорость анимации
-        if (current_frame > 3) current_frame -= 3; //проходимся по кадрам с первого по третий включительно. если пришли к третьему кадру - откидываемся назад.
-        m_actor.herosprite.setTextureRect(IntRect(96 * int(current_frame), 0, 96, 96)); //проходимся по координатам Х. получается 96,96*2,96*3 и опять 96
+        if (current_frame > 2) current_frame -= 2; //проходимся по кадрам с первого по третий включительно. если пришли к третьему кадру - откидываемся назад.
+        m_actor.herosprite.setTextureRect(IntRect(16 , 105 * int(current_frame), 82 , 90)); //проходимся по координатам Х. получается 96,96*2,96*3 и опять 96
         //std::cout << "dddddddddddddddddd" << "\n";
         // std::cout << current_frame << "BLABLA\n";
 

f1ca2d48321986e6767588c655878fc651f26e95
Denis <kdv.levart@gmail.com>
2020-05-26 13:21:31 +0300

    Initial version of interface

--- game/client/Player.cpp
+++ game/client/Player.cpp
@@ -10,10 +10,38 @@ Player::Player(sf::Uint64 id, const std::string& username, const sf::Vector2f& p
         , m_username(username)
         , m_actor("../../images/pigcop_vest.png")
         {
+    m_skull_texture.loadFromFile("../../client/inGameTextures/skull.png");
+    m_hp_bar_texture.loadFromFile("../../client/inGameTextures/hp_bar.png");
+    m_font.loadFromFile("../../client/menuTextures/font.ttf");
+    m_skull_sprite.setTexture(m_skull_texture);
+    m_hp_bar_sprite.setTexture(m_hp_bar_texture);
 }
 
 void Player::draw(sf::RenderWindow& window, float time, float& current_frame) {
     //std::cout << "ID " << get_id() << " X " << m_position.x << " Y " << m_position.y << "\n";
+
+    sf::Text kills_text;
+    kills_text.setFont(m_font);
+    kills_text.setFillColor(sf::Color::Red);
+    kills_text.setString("X       " + std::to_string(kills_count));
+
+    sf::Text hp_text;
+    hp_text.setFont(m_font);
+    if ((double) m_hp / m_max_hp > 0.8)
+        hp_text.setFillColor(sf::Color::Green);
+    else if ((double) m_hp / m_max_hp > 0.35)
+        hp_text.setFillColor(sf::Color::Yellow);
+    else
+        hp_text.setFillColor(sf::Color::Red);
+
+
+    hp_text.setString(std::to_string(m_hp));
+
+    hp_text.setPosition(m_position.x + 290, m_position.y + 195);
+    m_hp_bar_sprite.setPosition(m_position.x + 260, m_position.y + 200);
+
+    kills_text.setPosition(m_position.x + 230, m_position.y - 120);
+    m_skull_sprite.setPosition(m_position.x + 250, m_position.y - 120);
 
 
     hp_bar_green.setPosition(m_position.x + 50 - hp_bar_length / 2, m_position.y - 20);
@@ -30,6 +58,11 @@ void Player::draw(sf::RenderWindow& window, float time, float& current_frame) {
     window.draw(m_actor.herosprite);
     window.draw(hp_bar_green);
     window.draw(hp_bar_red);
+    window.draw(m_skull_sprite);
+    window.draw(kills_text);
+    window.draw(m_hp_bar_sprite);
+    window.draw(hp_text);
+
 }
 
 void Player::draw_stat(sf::RenderWindow& window) {
